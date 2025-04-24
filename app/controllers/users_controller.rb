2c2a2fcac69fcec32f50fd9c66d67f360cc5fa49
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2025-04-24 15:24:11 +0900

    feat: Implements ability to update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    params[:user].delete_if { |key, value| value.blank? }
+
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
